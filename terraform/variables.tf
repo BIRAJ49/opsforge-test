bf89cc62b99284839ffd9f23f039d590ea6ddba4
BIRAJ49 <124259795+BIRAJ49@users.noreply.github.com>
2026-05-10 22:13:37 +0545

    OpsForge: add terraform/variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" { default = "replace-with-aws-region" }
+variable "ami_id" { default = "replace-with-ami-id" }
+variable "instance_type" { default = "replace-with-instance-type" }
+variable "key_pair_name" { default = "replace-with-key-pair-name" }
+variable "vpc_id" { default = "replace-with-vpc-id" }
+variable "subnet_id" { default = "replace-with-subnet-id" }
